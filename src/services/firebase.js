d755b5f8306cb6401654dcc4cc9e178faafd1195
Frank Kelly <frankintech00@gmail.com>
2023-08-02 01:07:10 +0100

    setup folder structure

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,8 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
-import { getStorage } from 'firebase/storage';
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -15,9 +16,8 @@ const firebaseConfig = {
   storageBucket: "scran-vite-2b11a.appspot.com",
   messagingSenderId: "369718663107",
   appId: "1:369718663107:web:00c9f91c826505b65b0f76",
-  measurementId: "G-JVS26LETH9"
+  measurementId: "G-JVS26LETH9",
 };
-
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
@@ -26,4 +26,4 @@ export const db = getFirestore(app);
 export const auth = getAuth(app);
 export const storage = getStorage(app);
 export const provider = new GoogleAuthProvider();
-provider.setCustomParameters({ prompt: 'select_account' });
+provider.setCustomParameters({ prompt: "select_account" });
