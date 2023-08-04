f21c7a8ff43db4d1f7415058569d0b546cabdf8d
Frank Kelly <frankintech00@gmail.com>
2023-08-04 02:50:26 +0100

    Added ErrorMessage component

---
+++ src/components/common/ErrorMessage.jsx
@@ -0,0 +1,10 @@
+import { useContext } from "react";
+
+import { UserContext } from "../../contexts/UserContext";
+
+function ErrorMessage() {
+  const { error } = useContext(UserContext);
+  return <>{error && <p className="my-5 text-error">{error}</p>} </>;
+}
+
+export default ErrorMessage;
