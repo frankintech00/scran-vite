c8e7b9d3bf37804f541f7219fd8ca9672b22a30f
Frank Kelly <frankintech00@gmail.com>
2023-08-11 09:27:24 +0100

    added manifest and meta tags

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,34 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({ registerType: "autoUpdate" })],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Scran | A Recipe App for the Web",
+        short_name: "Scran",
+        description:
+          "Scran is a web application that allows users to search for recipes, add their own recipes, and create a shopping list based on the recipes they have selected. It was created as part of a Masters project for the MSc in Software Development at the University of Starthclyde.",
+        icons: [
+          {
+            src: "/assets/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/assets/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+        start_url: "/",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone",
+      },
+    }),
+  ],
   esbuild: {
     jsxFactory: "React.createElement",
     jsxFragment: "React.Fragment",
