214c9c2fd77bee722a9d64354521b2c57af61272
Frank Kelly <frankintech00@gmail.com>
2023-08-16 21:00:25 +0100

    added algolia search

--- src/components/header/SearchInput.jsx
+++ src/components/header/SearchInput.jsx
@@ -1,18 +1,41 @@
-import { FaSearch } from "react-icons/fa";
+import { useContext, useEffect } from "react";
+import { RecipeContext } from "../../contexts/RecipeContext";
+import algoliasearch from "algoliasearch/lite";
+import { InstantSearch, SearchBox, useHits } from "react-instantsearch";
+
+const searchClient = algoliasearch(
+  "5JL4NAA3E7",
+  "aec35c3c0d6a43fca6699b1e949f7db3"
+);
+
+function HitsComponent() {
+  const { setRecipes } = useContext(RecipeContext);
+  const { hits } = useHits();
+
+  useEffect(() => {
+    setRecipes(hits);
+  }, [hits, setRecipes]);
+
+  return null;
+}
 
 function SearchInput() {
   return (
-    <div className="w-full form-control">
-      <div className="relative">
-        <input
-          className="w-full py-2 pl-10 input input-bordered input-primary"
-          placeholder="Search for a recipe..."
-        />
-        <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-          <FaSearch className="text-primary" />
-        </div>
-      </div>
-    </div>
+    <InstantSearch searchClient={searchClient} indexName="scran-vite">
+      <SearchBox
+        searchAsYouType={true}
+        submitIconComponent={() => (
+          <div className="btn btn-primary rounded-l-none">Search</div>
+        )}
+        classNames={{
+          root: "w-full",
+          input: "input input-primary rounded-r-none",
+          reset: "hidden",
+        }}
+        placeholder="Search for a recipe..."
+      />
+      <HitsComponent />
+    </InstantSearch>
   );
 }
 
